43ac72f7546c75dd2297925818b634187521ecb2
Desor Guishny <desorguishny@gmail.com>
2022-12-06 18:10:43 -0500

    Update production for master key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.routes.default_url_options = {
-  host: 'https://jeepbook.herokuapp.com/api/v1/'
-}
+# Rails.application.routes.default_url_options = {
+#   host: 'https://jeepbook.herokuapp.com/api/v1/'
+# }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -23,7 +23,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
